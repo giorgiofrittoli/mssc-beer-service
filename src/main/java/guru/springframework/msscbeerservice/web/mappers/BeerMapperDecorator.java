fdd4dc4f16cca0d85c553dfcb0c95dc4bab51580
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-05-17 10:48:05 +0200

    comn with inventory service to get qty on hand

---
+++ src/main/java/guru/springframework/msscbeerservice/web/mappers/BeerMapperDecorator.java
@@ -0,0 +1,34 @@
+package guru.springframework.msscbeerservice.web.mappers;
+
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.services.inventory.BeerInventoryService;
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+//decorate int beer mapper to add beer qty
+public abstract class BeerMapperDecorator implements BeerMapper {
+
+    private BeerInventoryService beerInventoryService;
+    private BeerMapper beerMapper;
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+        //map to dto with gen mapper
+        BeerDto dto = beerMapper.beerToBeerDto(beer);
+        //call inv service to retrieve qty
+        dto.setQuantityOnHand(beerInventoryService.getOnHandInventory(beer.getId()));
+        return dto;
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto dto) {
+        return beerMapper.beerDtoToBeer(dto);
+    }
+
+    //to inject generated mapper
+    @Autowired
+    public void setBeerMapper(BeerMapper beerMapper) {
+        this.beerMapper = beerMapper;
+    }
+}
