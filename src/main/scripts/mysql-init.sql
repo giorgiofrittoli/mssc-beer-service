da847abbd5b1a62b23f1d577f139ed2b5ea3d4ad
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-05-28 22:29:50 +0200

    mysql init script

---
+++ src/main/scripts/mysql-init.sql
@@ -0,0 +1,7 @@
+drop database if exists beerservice;
+drop user if exists 'beer_service'@'%';
+create database if not exists beerservice character set utf8mb4 collate utf8mb4_unicode_ci;
+create user if not exists 'beer_service'@'%' identified with mysql_native_password by 'password';
+grant select,insert,update,delete,create,drop,references,index,alter,execute,create view,show view,create routine,
+ alter routine,event,trigger on beerservice.* to 'beer_service'@'%';
+flush privileges;
