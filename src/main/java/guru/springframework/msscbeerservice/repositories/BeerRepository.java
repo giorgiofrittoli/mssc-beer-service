44fe62691de8f8a35d212470ba9b020ee3ca8f34
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-02 14:35:17 +0200

    scheduled task for check inventory

--- src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -1,11 +1,10 @@
 package guru.springframework.msscbeerservice.repositories;
 
 import guru.springframework.msscbeerservice.domain.Beer;
-import guru.springframework.msscbeerservice.web.model.BeerDto;
 import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -13,7 +12,7 @@ import java.util.UUID;
 /**
  * Created by jt on 2019-05-17.
  */
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
 
     Page<Beer> findAllByBeerName(String beerName, PageRequest pageRequest);
