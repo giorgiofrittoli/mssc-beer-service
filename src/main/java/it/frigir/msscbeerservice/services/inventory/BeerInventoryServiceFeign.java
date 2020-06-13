85abc9f9b6f3cf94e370ad3a8987c48b0b78e2bb
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-06-13 19:57:10 +0200

    beer inventory service with eureka

---
+++ src/main/java/it/frigir/msscbeerservice/services/inventory/BeerInventoryServiceFeign.java
@@ -0,0 +1,39 @@
+package it.frigir.msscbeerservice.services.inventory;
+
+import it.frigir.brewery.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Slf4j
+@RequiredArgsConstructor
+@Profile("local-discovery")
+@Service
+public class BeerInventoryServiceFeign implements BeerInventoryService {
+
+    private final InventoryServiceFeignClient inventoryServiceFeignClient;
+
+    @Override
+    public Integer getOnHandInventory(UUID beerId) {
+
+        log.debug("Calling inventory client - beerId " + beerId);
+
+        ResponseEntity<List<BeerInventoryDto>> responseEntity = inventoryServiceFeignClient.getOnHandInventory(beerId);
+
+        Integer qtyOnHand = Objects.requireNonNull(responseEntity.getBody())
+                .stream()
+                .mapToInt(BeerInventoryDto::getQuantityOnHand)
+                .sum();
+
+        log.debug("BeerId[" + beerId + "], on hand is " + qtyOnHand);
+
+        return qtyOnHand;
+
+    }
+}
