021f167f267d1ff5c49ad3d34aaeeff2e817a68e
Giorgio Frittoli <giorgio.frittoli@gmai.com>
2020-07-15 17:33:43 +0200

    fix feign fallback client

--- src/main/java/it/frigir/msscbeerservice/services/inventory/InventoryServiceFeignClientFailover.java
+++ src/main/java/it/frigir/msscbeerservice/services/inventory/InventoryServiceFeignClientFailover.java
@@ -6,16 +6,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
-public class InventoryServiceFeignClientFailover implements InventoryServiceFeignClient {
+public class InventoryServiceFeignClientFailover implements InventoryFailoverFeignClient {
 
     private final InventoryFailoverFeignClient inventoryFailoverFeignClient;
 
     @Override
-    public ResponseEntity<List<BeerInventoryDto>> getOnHandInventory(UUID beerId) {
+    public ResponseEntity<List<BeerInventoryDto>> getOnHandInventory() {
         return inventoryFailoverFeignClient.getOnHandInventory();
     }
 }
